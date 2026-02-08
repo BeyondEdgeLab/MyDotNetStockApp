f5408ace9df175dbf14d9bf925259421b0e54a3f
atash <atash@LAPTOP-R2LDVLRR>
2026-02-07 20:04:18 -0600

    addswagger

---
+++ StockApp/Program.cs
@@ -0,0 +1,27 @@
+using StockApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IStockService, YahooFinanceStockService>();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
